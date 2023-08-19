e94e481c07121788252e0fb7fecad4d82eaa212f
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-19 08:18:07 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
